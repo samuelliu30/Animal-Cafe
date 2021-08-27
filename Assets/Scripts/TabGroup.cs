9c30bbae6090ef06a4bcab18010ac79c02dcdf0f
samuelliu30 <Yichen.Liu@amd.com>
2021-08-27 19:18:35 -0400

    UI changes: New interface design

---
+++ Assets/Scripts/TabGroup.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<TabButton> tabButtons;
+    public Sprite tabIdle;
+    public Sprite tabActive;
+    public TabButton selectedTab;
+    public List<GameObject> objectsToSwap;
+
+    public void Subscribe(TabButton button)
+    {
+        if(tabButtons == null)
+        {
+            tabButtons = new List<TabButton>();
+        }
+
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButton button)
+    {
+        ResetTabs();
+
+    }
+
+    public void OnTabExit(TabButton button)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(TabButton button)
+    {
+        selectedTab = button;
+        ResetTabs();
+        button.background.sprite = tabActive;
+    }
+
+    public void ResetTabs()
+    {
+        foreach(TabButton button in tabButtons)
+        {
+            if(selectedTab != null && button == selectedTab) { continue; }
+            button.background.sprite = tabIdle;
+        }
+    }
+
+}
